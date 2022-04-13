69ae3afc539e6f7cf5c039955ff59eaaf30f25fd
George Avsetsin <mail@sjors.ru>
2022-04-13 16:49:07 +0300

    feat: remove balancer & sushi rewards programs

--- test/manifest.e2e-spec.ts
+++ test/manifest.e2e-spec.ts
@@ -23,8 +23,6 @@ const MANIFESTS_DIR = `${__dirname}/../manifests/mainnet/`;
 
 describe.each([
   { name: 'Curve', fileName: 'curve.json', startBlock: 13203786 },
-  { name: 'Sushi Swap', fileName: 'sushi-swap.json', startBlock: 13625895 },
-  { name: 'Balancer v2', fileName: 'balancer-v2.json', startBlock: 14246088 },
 ])('$name', ({ fileName, startBlock }) => {
   let providerService: ProviderService;
   let loaderService: LoaderService;
